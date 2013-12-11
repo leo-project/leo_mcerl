00510c50b58d16e45753f43e17fc06e24298e476
Yosuke Hara <yousuke.hara@gmail.com>
2013-12-11 17:59:01 +0900

    Upgrade libcutil to 0.4.2

--- c_src/build_deps.sh
+++ c_src/build_deps.sh
@@ -8,7 +8,7 @@ if [ `uname -s` = 'SunOS' -a "${POSIX_SHELL}" != "true" ]; then
 fi
 unset POSIX_SHELL # clear it so if we invoke other scripts, they run as ksh as well
 
-LIBCUTIL_VSN="0.4.1"   # libcutil master Nov 26, 2013
+LIBCUTIL_VSN="0.4.2"   # libcutil master Dec 11, 2013
 
 set -e
 
