668020096cbf12305dc5245e5f3c97c86521afff
yosuke <yosuke@ubuntu1804.intra.rakuten.co.jp>
2018-04-03 15:43:23 +0900

    Bump libcutil

--- c_src/build_deps.sh
+++ c_src/build_deps.sh
@@ -8,7 +8,7 @@ if [ `uname -s` = 'SunOS' -a "${POSIX_SHELL}" != "true" ]; then
 fi
 unset POSIX_SHELL # clear it so if we invoke other scripts, they run as ksh as well
 
-LIBCUTIL_VSN="0.4.5"   # libcutil master Aug 18, 2017
+LIBCUTIL_VSN="0.5.0"   # libcutil master April 3, 2018
 
 set -e
 
